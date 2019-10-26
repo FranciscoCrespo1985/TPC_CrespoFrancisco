d158796ff9b516c943d723f3643b411acf03da52
FranciscoCrespo1985 <gpacomail@gmail.com>
2019-10-26 18:11:48 -0300

    update

---
+++ Club/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Club.Models
+{
+    public class Usuario
+    {
+        public long ID { get; set; }
+        public string EMAIL { get; set; }
+        public string PWD { get; set; }
+    }
+}
