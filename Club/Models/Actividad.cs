a354f0ba7d2c466ee2878e41e1f2ba067dfc9a28
FranciscoCrespo1985 <gpacomail@gmail.com>
2019-10-26 18:12:19 -0300

    update

--- Club/Models/Actividad.cs
+++ Club/Models/Actividad.cs
@@ -7,5 +7,19 @@ namespace Club.Models
 {
     public class Actividad
     {
+        public long ID { get; set; }
+        public long ID_ACTIVIDAD_TIPO { get; set; }
+        public long ID_SOCIO { get; set; }
+        public long ID_PROFESOR { get; set; }
+        public long ID_LOCACION { get; set; }
+
+        public DateTime FECHA { get; set; }
+
+        public bool ES_CANCELADO { get; set; }
+        public bool ES_APROBADO { get; set; }
+        public bool ES_FINALIZADO { get; set; }
+
+
+
     }
 }
